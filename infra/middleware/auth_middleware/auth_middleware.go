3c5e1047dd8a8e612b9deb220c38b530d92da414
rulyadhika <rulihanif@gmail.com>
2024-04-18 23:22:15 +0700

    feat: add role authorization middleware

--- infra/middleware/auth_middleware/auth_middleware.go
+++ infra/middleware/auth_middleware/auth_middleware.go
@@ -1,6 +1,8 @@
 package authmiddleware
 
 import (
+	"log"
+	"slices"
 	"strings"
 
 	"github.com/gin-gonic/gin"
@@ -11,6 +13,7 @@ import (
 
 type AuthMiddleware interface {
 	Authentication() gin.HandlerFunc
+	RoleAuthorization(rolesAllowed []string) gin.HandlerFunc
 }
 
 type AuthMiddlewareImpl struct{}
@@ -47,6 +50,35 @@ func (a *AuthMiddlewareImpl) Authentication() gin.HandlerFunc {
 		}
 
 		ctx.Set("userData", userData)
+		ctx.Next()
+	}
+}
+
+func (a *AuthMiddlewareImpl) RoleAuthorization(rolesAllowed []string) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userData, ok := ctx.MustGet("userData").(*jwt.JWTPayload)
+
+		_ = userData
+
+		if !ok {
+			log.Printf("[RoleAuthorization - Middleware] err: %s\n", "failed type casting to '*jwt.JWTPayload'")
+			internalServerErr := errs.NewInternalServerError("something went wrong")
+			ctx.AbortWithStatusJSON(internalServerErr.StatusCode(), internalServerErr)
+			return
+		}
+
+		userIsEligible := []bool{}
+
+		for _, r := range userData.Roles {
+			userIsEligible = append(userIsEligible, slices.Contains(rolesAllowed, r))
+		}
+
+		if !slices.Contains(userIsEligible, true) {
+			forbiddenErr := errs.NewForbiddenError("you are not authorized to access/modify this data")
+			ctx.AbortWithStatusJSON(forbiddenErr.StatusCode(), forbiddenErr)
+			return
+		}
+
 		ctx.Next()
 	}
 }
